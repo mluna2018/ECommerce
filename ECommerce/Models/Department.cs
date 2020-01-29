8eb623b686ee42522498489fcfabe05398166fe8
mluna2018 <44854554+mluna2018@users.noreply.github.com>
2020-01-29 15:11:48 -0500

    avanceEcommerce

---
+++ ECommerce/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage ="El campo  {0} es requerido.")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener maximo {1} caracteres.")]
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ ECommerce/Models/ECommerceContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class ECommerceContext : DbContext
+    {
+        public ECommerceContext() : base ("DefaultConnection")
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
